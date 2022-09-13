7adbafdcda6125a3e05522bd9dd814933e3bf95b
basakk123 <basakk@naver.com>
2022-09-13 16:06:34 +0900

    보드서비스 숙제

--- src/main/java/site/metacoding/red/service/BoardsService.java
+++ src/main/java/site/metacoding/red/service/BoardsService.java
@@ -5,4 +5,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class BoardsService {
 
+	public void 게시글목록보기(Integer page, String keyword) {
+		//페이징 해야됨
+	}
+	
+	public void 게시글상세보기(Integer id) {
+		
+	}
+	
+	public void 게시글수정하기(Integer id) {
+		// 디티오 매개변수에 추가
+	}
+	
+	public void 게시글삭제하기() {
+		
+	}
+	
+	public void 게시글쓰기() {
+		
+	}
 }
